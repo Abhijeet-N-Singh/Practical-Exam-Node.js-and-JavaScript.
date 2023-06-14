93eff9c2215953b42d2371b22779916beeabcede
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-14 13:05:07 +0530

    Question 1.js

---
+++ Question 1.js
@@ -0,0 +1,7 @@
+Question 1) what is Node.js ?
+Answer - Node.js is an open source server enviornment.
+Node.js runs on various platforms (windows, Linux, Unix, Mac OperatingSystem X, etc.).
+Node.js is free and 
+Node.js uses JavaScript on the server.
+Node.js uses asynchronous programming, that leads to 
+ run multiple tasks without waiting one for to finish !
