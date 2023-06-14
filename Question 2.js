ea1448e64b6ed34d65b90e2e86b5ccee77889190
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-14 13:06:11 +0530

    Question 2.js

---
+++ Question 2.js
@@ -0,0 +1,5 @@
+ Question 2) What is Module in Node.js ?
+// Answer - Modules can be a set of functions we want // to include in our application.
+// Consider modules to be the same as JavaScript or
+// Python libraries.
+// Node.js has a set of built-in modules which we can // use without any further installation.
